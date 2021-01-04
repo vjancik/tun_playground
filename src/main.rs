9a3829b7e497bfe9e97a2182569b6630765180aa
Viktor Jancik <viktor.vix.jancik@gmail.com>
2021-01-05 00:07:13 +0100

    Functional naive UDP tunnel implementation

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 // https://www.kernel.org/doc/Documentation/networking/tuntap.txt
 // https://github.com/torvalds/linux/blob/master/include/uapi/linux/if_tun.h
 
-use std::{ptr, io, net, os::unix::io::AsRawFd};
+use std::{ptr, io, net};
 use nix::{unistd, errno};
 use anyhow::{Result};
 use mio;
@@ -42,7 +42,8 @@ fn main() -> Result<()> {
     let tun_mask = matches.value_of("mask").unwrap();
 
     let is_server = matches.is_present("server");
-    let udp_addr = matches.value_of("public").unwrap().parse::<net::SocketAddr>()?;
+    let server_addr = matches.value_of("public").unwrap().parse::<net::SocketAddr>()?;
+    let mut client_addr: net::SocketAddr = "0.0.0.0:0".parse()?; // filled on first packet
 
     // let mut addr_dest_buf = [0u8; 4];
     let tun_iff = tun::Tun::new(tun_name.to_owned())?
@@ -65,41 +66,58 @@ fn main() -> Result<()> {
     use socket2::{Socket, Domain, Type, Protocol};
     let udp_sock = Socket::new(Domain::ipv4(), Type::dgram().non_blocking(), Some(Protocol::udp()))?;
     udp_sock.set_reuse_address(true)?;
-    udp_sock.set_reuse_port(true)?;
-    if is_server {
-        let port = udp_addr.port();
-        udp_sock.bind(&format!{"127.0.0.1:{}", port}.parse::<net::SocketAddr>()?.into())?;
-    } else {
-        udp_sock.bind(&"127.0.0.1:0".parse::<net::SocketAddr>()?.into())?;
-    }
-    // let mut udp_sock = mio::net::UdpSocket::from_std(udp_sock.into_udp_socket());
 
-    poll.registry().register(&mut mio::unix::SourceFd(&udp_sock.as_raw_fd()), PUBLIC_IFF, mio::Interest::READABLE)?;
+    if is_server {
+        let port = server_addr.port();
+        udp_sock.bind(&format!{"0.0.0.0:{}", port}.parse::<net::SocketAddr>()?.into())?;
+    } else {
+        udp_sock.bind(&"0.0.0.0:0".parse::<net::SocketAddr>()?.into())?;
+    }
+    let mut udp_sock = mio::net::UdpSocket::from_std(udp_sock.into_udp_socket());
+
+    poll.registry().register(&mut udp_sock, PUBLIC_IFF, mio::Interest::READABLE)?;
 
     loop {
         poll.poll(&mut events, None)?;
 
         for event in events.iter() {
-            if event.token() == TUN_IFF { 
-                let read_result = unistd::read(tun_iff.fd, &mut buf);
-                if read_result == Err(nix::Error::Sys(errno::EWOULDBLOCK)) {
-                    println!("Read would block");
-                    continue;
+            if event.token() == TUN_IFF {
+                loop {
+                    let nread = match unistd::read(tun_iff.fd, &mut buf) {
+                        Err(nix::Error::Sys(errno::EWOULDBLOCK)) => break,
+                        any => any,
+                    }?;
+
+                    // println!("Writing {} bytes from TUN to UDP socket", nread);
+                    // TODO: Address table for multiple clients
+                    let peer_addr = match is_server {
+                        true => client_addr,
+                        false => server_addr
+                    };
+            
+                    let _nwrite = match udp_sock.send_to(&mut buf[..nread], peer_addr) {
+                        Err(error) if error.kind() == io::ErrorKind::WouldBlock => break,
+                        any => any,
+                    }?;
                 }
-                let nread = read_result?;
-
-                // icmp_handler(&mut buf[..nread]);
-
-                println!("Writing {} bytes from TUN to UDP socket", nread);
-                let _nwrite = udp_sock.send_to(&mut buf[..nread], &udp_addr.into())?;
             } else {
-                let (nread, _) = match udp_sock.recv_from(&mut buf) {
-                    Err(error) if error.kind() == io::ErrorKind::WouldBlock => continue,
-                    any => any,
-                }?;
+                loop {
+                    let (nread, addr) = match udp_sock.recv_from(&mut buf) {
+                        Err(error) if error.kind() == io::ErrorKind::WouldBlock => break,
+                        any => any,
+                    }?;
 
-                println!("Writing {} bytes from UDP socket to TUN", nread);
-                let _nwrite = unistd::write(tun_iff.fd, &mut buf[..nread])?;
+                    #[allow(unused_assignments)]
+                    if is_server {
+                        client_addr = addr;
+                    }
+
+                    // println!("Writing {} bytes from UDP socket to TUN", nread);
+                    let _nwrite = match unistd::write(tun_iff.fd, &mut buf[..nread]) {
+                        Err(nix::Error::Sys(errno::EWOULDBLOCK)) => break,
+                        any => any,
+                    }?;
+                }
             }
         }
     }
